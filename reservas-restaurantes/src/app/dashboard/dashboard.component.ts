9b68993b02eb614cd8668f70862b8187e45b36cd
ShoniG19 <jonigodoy19.jg@gmail.com>
2025-11-16 18:10:35 -0300

    Actualizar rutas de imágenes en el componente de dashboard y agregar nuevos íconos para mejorar la gestión de reservas

--- reservas-restaurantes/src/app/dashboard/dashboard.component.ts
+++ reservas-restaurantes/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  imports: [CommonModule, RouterModule],
+  standalone: true,
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent{}
